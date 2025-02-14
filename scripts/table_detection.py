e93798f5687d0f12a7e7083d1dd7f0dd57a09772
Pun <impan_1611_2544@hotmail.com>
2025-02-14 09:11:13 +0700

    Create : ocr v6 hs and tn

---
+++ scripts/table_detection.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+from pathlib import Path
+
+output_folder = Path("../data/output_images/output_V6")
+
+def split_grade_table_and_students(binary_img):
+    # แยกตาราง
+    num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(binary_img, connectivity=8)
+    areas = [stat[4] for stat in stats]  # ดึงค่า area
+    sorted_areas = sorted(areas, reverse=True)  # เรียงลำดับจากมากไปน้อย
+    second_max_area = sorted_areas[1]  # ค่าอันดับ 2
+    second_max_area_index = areas.index(second_max_area)  # หาตำแหน่งในลิสต์เดิม
+    table_position = stats[second_max_area_index]
+    x, y, w, h, area = table_position
+    table_img = binary_img[y:y+h, x:x+w]
+    
+    # ข้อมูลนักเรียน
+    new_x = int((x+w) * 0.74)
+    student_info_img = binary_img[:y, :new_x]
+
+    return table_img, student_info_img
+
+def biggest_contour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        area = cv2.contourArea(i)
+        #print(area)
+        if area > 1000:
+            #print("มา")
+            peri = cv2.arcLength(i, True)
+            approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+
+    return biggest
+
+def persective_transformation(table_original_img, table_binary_img):
+
+    # ค้นหาคอนทัวร์
+    contours, hierarchy = cv2.findContours(table_binary_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = sorted(contours, key=cv2.contourArea, reverse=True)[:10]
+
+    # ค้นหาสี่เหลี่ยมที่ใหญ่ที่สุด
+    biggest = biggest_contour(contours)
+
+    points = biggest.reshape(4, 2)
+    input_points = np.zeros((4, 2), dtype="float32")
+
+    points_sum = points.sum(axis=1)
+    input_points[0] = points[np.argmin(points_sum)]
+    input_points[3] = points[np.argmax(points_sum)]
+
+    points_diff = np.diff(points, axis=1)
+    input_points[1] = points[np.argmin(points_diff)]
+    input_points[2] = points[np.argmax(points_diff)]
+
+    (top_left, top_right, bottom_right, bottom_left) = input_points
+
+    # Euclidean Distance Formula
+    bottom_width = np.sqrt(((bottom_right[0] - bottom_left[0]) ** 2) + ((bottom_right[1] - bottom_left[1]) ** 2))
+    top_width = np.sqrt(((top_right[0] - top_left[0]) ** 2) + ((top_right[1] - top_left[1]) ** 2))
+    rigth_height = np.sqrt(((top_left[0] - bottom_right[0]) ** 2) + ((top_left[1] - bottom_right[1]) ** 2))
+    left_height = np.sqrt(((top_left[0] - bottom_left[0]) ** 2) + ((top_left[1] - bottom_left[1]) ** 2))
+
+    # Output image size
+    #max_width = max(int(bottom_width), int(top_width))
+    expand_width = round(max(int(bottom_width), int(top_width)) * 0.4)
+    max_width = max(int(bottom_width), int(top_width)) + expand_width
+    max_height = max(int(rigth_height), int(left_height))
+
+    # Desird points values in the output image
+    converted_points = np.float32([[0, 0], [max_width, 0], [0, max_height], [max_width, max_height]])
+
+    # Perspective transformaxtion
+    matrix = cv2.getPerspectiveTransform(input_points, converted_points)
+    img_out = cv2.warpPerspective(table_binary_img.copy(), matrix, (max_width, max_height))
+
+    return img_out
+
+def add(a, b):
+    return a + b
+
+
